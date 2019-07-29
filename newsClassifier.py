af7b53cd7fa269158762d3a83bead6767aa4d033
Protick <kprotickju22@gmail.com>
2019-07-29 23:44:03 +0600

    add jupyter notebook

--- newsClassifier.py
+++ newsClassifier.py
@@ -25,13 +25,21 @@ for i in range(len(X)):
     
     
 #for feature extraction using CountVectorizer and then tensform it Tfidf model
+    '''
 from sklearn.feature_extraction.text import CountVectorizer 
-vectorize = CountVectorizer(min_df=2,max_df=0.7);
+vectorize = CountVectorizer(min_df=3,max_df=0.4);
 X =  vectorize.fit_transform(corpus).toarray()
 
 from sklearn.feature_extraction.text import TfidfTransformer
 transforme = TfidfTransformer()
 X = transforme.fit_transform(X).toarray()
+
+'''
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(max_features=370,min_df=3,max_df=0.6)
+X = vectorizer.fit_transform(corpus).toarray()
+
+
 
 
 
@@ -56,7 +64,7 @@ classifier.score(text_test,label_test)
 
 from sklearn.metrics import accuracy_score
 
-print ("LogisticRegression accuracy score : ",accuracy_score(label_pred,label_test))
+print ("LogisticRegression accuracy score : ",accuracy_score(label_pred,label_test)*100)
 
 
 from sklearn.naive_bayes import MultinomialNB
@@ -69,7 +77,7 @@ label_pred_nb = nbclf.predict(text_test)
 
 cmNB = confusion_matrix(label_test,label_pred_nb)
 
-print ("Naive Bayes accuracy score : ",accuracy_score(label_pred_nb,label_test))
+print ("Naive Bayes accuracy score : ",accuracy_score(label_pred_nb,label_test)*100)
 
 from sklearn.ensemble import RandomForestClassifier
 rfCLF = RandomForestClassifier(n_estimators=160)
@@ -79,14 +87,41 @@ label_pred_rf = rfCLF.predict(text_test)
 
 cmRF = confusion_matrix(label_test,label_pred_rf)
 
-print ("RandomForestClassifier accuracy score : ",accuracy_score(label_pred_rf,label_test))
+print ("RandomForestClassifier accuracy score : ",accuracy_score(label_pred_rf,label_test)*100)
 
 from sklearn.svm import SVC
 svclassifier = SVC(kernel='linear')
 svclassifier.fit(text_train,label_train)
 label_pred_svm = svclassifier.predict(text_test)
 cmSVM = confusion_matrix(label_test,label_pred_svm)
-print ("SVM accuracy score : ",accuracy_score(label_pred_svm,label_test))
+print ("SVM accuracy score : ",accuracy_score(label_pred_svm,label_test)*100)
+
+
+
+
+# Pickleing the classifier
+with open('classifier.pickle','wb') as f:
+    pickle.dump(classifier,f)
+
+# pickleing the Tfidfmodel
+    
+with open('tifdf.pickle','wb') as f:
+    pickle.dump(vectorizer,f)
+    
+with open('classifier.pickle','rb') as f:
+    clf = pickle.load(f)
+
+with open('tifdf.pickle','rb') as f:
+    tfidf = pickle.load(f)
+    
+sample = ['ফেসবুকের পাঁচ শ কোটি ডলার জরিমানা']
+
+sample = tfidf.transform(sample).toarray()
+
+print(clf.predict(sample))
+
+
+
 
 
 
